7ad0a9727a9e846bbba67fb4a9b27127e39dcfe2
ivan295 <rafaelivan1995@hotmail.com>
2022-04-10 12:22:06 -0500

    initial commit

---
+++ src/main/java/utm/edu/aplicacion1/Aplicacion1Application.java
@@ -0,0 +1,13 @@
+package utm.edu.aplicacion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplicacion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplicacion1Application.class, args);
+	}
+
+}
---
+++ src/test/java/utm/edu/aplicacion1/Aplicacion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package utm.edu.aplicacion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aplicacion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
